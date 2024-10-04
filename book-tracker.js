f389a8110e7959df8a3398dc784ea0d4ea6cb17a
Hayden Dingman <haydendingman@gmail.com>
2024-10-04 12:34:03 -0700

    Add Book constructor

---
+++ book-tracker.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title,
+    this.author= author,
+    this.pages = pages,
+    this.isRead = isRead
+}
+
+function addBookToLibrary() {
+    
+}
